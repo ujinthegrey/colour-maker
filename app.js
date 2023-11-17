4ef0a5db6d13351cbec1f122fb2e281cf878d23b
ujinthegrey <v4deus@gmail.com>
2023-11-17 17:41:33 +0400

    add app.js, make functions for random change colors etc

---
+++ app.js
@@ -0,0 +1,57 @@
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', (event) => {
+    event.preventDefault()
+    if (event.code.toLowerCase() === 'space') {
+        setRandomColors()
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type.toLowerCase() === 'lock') {
+        const node =
+            event.target.tagName.toLowerCase() === 'i'
+                ? event.target
+                : event.target.children[0]
+
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+    }
+})
+
+function generateRandomColor() {
+    const hexCodes = '0123456789ABCDF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function setRandomColors() {
+    cols.forEach(col => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')        
+
+        if (isLocked) {
+            return
+        }
+
+        const color = chroma.random()
+        col.style.background = color
+        text.textContent = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
